73c509f966236410e01cea6fa60e5ce80b987fab
Duy <quocdup1@gmail.com>
2026-05-11 16:01:20 +0700

    change api

--- mobile-app/config/api.ts
+++ mobile-app/config/api.ts
@@ -7,8 +7,9 @@ const SERVER_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.31.197:300
 
 export const API_CONFIG = {
   //BASE_URL: process.env.EXPO_PUBLIC_API_URL || 'http://192.168.31.197:3000/api/v1',
-  //BASE_URL: 'https://healthguard-backend.onrender.com/api/v1',
-  BASE_URL: `${SERVER_URL}/api/v1`,
+  //BASE_URL: 'https://healthguard-api-42q2.onrender.com/api/v1.',
+  BASE_URL: 'https://healthguard-api-42q2.onrender.com/api/v1',
+  //BASE_URL: `${SERVER_URL}/api/v1`,
   TIMEOUT: 60000,
 };
 // Các endpoints (theo Swagger documentation)
